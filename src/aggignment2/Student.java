b11b1970b104e7a121d633af6d0dfc3641853241
Michael Snopko <michael.snopko@gmail.com>
2017-01-24 14:04:41 -0500

    Student JSON done!

---
+++ src/aggignment2/Student.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aggignment2;
+
+/**
+ *
+ * @author c0638820
+ */
+import java.util.Objects;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author c0638820
+ */
+public class Student {
+
+    private String name;
+    private String id;
+    private String gender;
+    private double grade;
+
+    public Student() {
+
+    }
+
+    public Student(String name, String id, String gender, double grade) {
+        this.name = name;
+        this.id = id;
+        this.gender = gender;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (Double.doubleToLongBits(this.grade) != Double.doubleToLongBits(other.grade)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.gender, other.gender)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        JSONObject json = new JSONObject();
+        json.put("name",name );
+        json.put("id", id);
+        json.put("gender", gender);
+        json.put("grade", grade);
+        return json.toJSONString();
+    }
+
+}
